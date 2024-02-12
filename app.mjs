2a3602f93f7661087b9b1d7294c7bc29a0367cda
Muhammad Ilyas <124496272+eDeveloper132@users.noreply.github.com>
2024-02-12 15:52:35 +0500

    incomplete

---
+++ app.mjs
@@ -0,0 +1,9 @@
+const data = document.querySelector(".form").value;
+const name = document.querySelector("#name");
+const mail = document.querySelector("#email");
+const pass = document.querySelector("#pwd");
+const CruidHandler = () =>
+{
+    console.log(data);
+}
+CruidHandler();
